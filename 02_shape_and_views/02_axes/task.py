dcb5fdbcaf6b8fc10e391abd2f7048ace08968ec
lvinnichenko <lvinnichenko@aligntech.com>
2026-08-08 12:45:52 +0300

    03_indexing is implemented

--- 02_shape_and_views/02_axes/task.py
+++ 02_shape_and_views/02_axes/task.py
@@ -3,4 +3,4 @@ import torch
 
 def channel_first(x: torch.Tensor) -> torch.Tensor:
     """Преобразовать HWC в NCHW."""
-    raise NotImplementedError
+    return x.permute(2, 0, 1).unsqueeze(0)
--- 03_indexing/01_slices/task.py
+++ 03_indexing/01_slices/task.py
@@ -3,4 +3,4 @@ import torch
 
 def interior_and_border(x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
     """Получить центр матрицы и развёрнутую верхнюю границу."""
-    raise NotImplementedError
+    return (x[1:-1, 1:-1], x[0].flip(0))
