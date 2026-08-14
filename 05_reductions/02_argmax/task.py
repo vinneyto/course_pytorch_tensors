fb670aa3c9439b782966f31c4445d154d1345205
lvinnichenko <lvinnichenko@aligntech.com>
2026-08-14 12:22:17 +0300

    implements up to 05/02

--- 05_reductions/02_argmax/task.py
+++ 05_reductions/02_argmax/task.py
@@ -3,4 +3,8 @@ import torch
 
 def best_in_each_row(scores: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
     """Вернуть индексы и значения победителей строк."""
-    raise NotImplementedError
+    # indices = scores.argmax(1)
+    # values = scores.gather(1, indices.unsqueeze(-1)).squeeze(1)
+    values, indices = scores.max(1)
+
+    return (indices, values)
